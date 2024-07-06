ffdbeeb8b686466c752b766e8370e2ffcd908b45
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-07-06 20:43:18 +0500

    add user

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import get_object_or_404
 
 from reviews.models import Category, Genre, Title, Review
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from .constants import MAX_NAME_LENGTH, MAX_SLUG_LENGTH, CHAR_OUTPUT_LIMIT
 
+
+
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=MAX_NAME_LENGTH)
@@ -36,10 +40,9 @@ class Title(models.Model):
 
 class Review(models.Model):
     text = models.TextField()
-    # author = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, related_name='reviews'
-    # )
-    author = models.IntegerField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
     score = models.IntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
     title = models.ForeignKey(
