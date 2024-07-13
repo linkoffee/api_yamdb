b82e82c738e16d53e36fb3fd141ca12acd160e09
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-07-13 11:56:26 +0500

    fix import_data script, delete json-config file

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,17 +1,25 @@
 import os
 import csv
-import json
 import logging
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
 
 from reviews import models
 
 # Путь до директории с csv-файлами:
-DIR_PATH = 'static/data'
+CSV_DIR_PATH = 'static/data/'
 
-# Файл конфигурации импорта:
-CONFIG_FILE = 'json/import_data_config.json'
+# Поля, которые являются внешними ключами.
+FOREIGN_KEY_FIELDS = ('author', 'category')
+
+# Словарь соответствий модели и csv-файла:
+MODEL_AND_CSV_MATCHING = {
+    models.Category: 'category.csv',
+    models.Comment: 'comments.csv',
+    models.Genre: 'genre.csv',
+    models.Review: 'review.csv',
+    models.Title: 'titles.csv',
+    models.User: 'users.csv'
+}
 
 # Настройка логирования:
 log_file_path = os.path.join(os.path.dirname(__file__), 'import_data.log')
@@ -29,76 +37,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_instance(model, pk):
-    """
-    Получает экземпляр модели по первичному ключу.
-    Возвращает None и логирует ошибку, если экземпляр не найден.
-    """
-    try:
-        return model.objects.get(pk=pk)
-    except model.DoesNotExist:
-        logger.error(
-            f'Instance of model {model.__name__} with pk {pk} does not exist.')
-        return None
-
-
-def transform_row_to_data(row, fields_mapping, model):
-    """
-    Преобразует строку CSV в словарь данных для модели.
-    Обрабатывает внешние ключи, такие как `category`.
-    """
-    data = {}
-    for key, value in row.items():
-        if key in fields_mapping:
-            field_name = fields_mapping[key]
-            if field_name == 'category' and model == models.Title:
-                data['category'] = get_instance(models.Category, value)
-            elif field_name == 'genre' and model == models.Title:
-                genres = [get_instance(models.Genre, genre_id)
-                          for genre_id in value.split(',')]
-                data['genre'] = [genre for genre in genres if genre is not None]
-            else:
-                data[field_name] = value
-    return data
-
-
-def process_csv_row(row, fields_mapping, model, unique_field):
-    """
-    Обрабатывает одну строку CSV, преобразуя её в данные модели.
-    Выполняет вставку или обновление записи.
-    """
-    data = transform_row_to_data(row, fields_mapping, model)
-    if None not in data.values() or 'genre' in data:
-        try:
-            instance, created = model.objects.update_or_create(
-                **{unique_field: data[unique_field]},
-                defaults={k: v for k, v in data.items() if k != 'genre'}
-            )
-            if 'genre' in data:
-                instance.genre.set(data['genre'])
-                instance.save()
-        except IntegrityError as e:
-            logger.error(f'IntegrityError importing data from CSV row: {e}')
-
-
-def import_data_from_csv(file_path, model, fields_mapping):
-    """
-    Читает CSV-файл и обрабатывает каждую строку.
-    Вызывая process_csv_row.
-    """
-    unique_field = fields_mapping.pop(
-        'unique_field')
-    try:
-        with open(file_path, 'r', encoding='utf-8') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                process_csv_row(row, fields_mapping, model, unique_field)
-    except KeyError as e:
-        logger.error(f'KeyError importing data from {file_path}: {e}')
-    except Exception as e:
-        logger.error(f'Error importing data from {file_path}: {e}')
-
-
 class Command(BaseCommand):
     """
     Пользовательская команда Django.
@@ -109,35 +47,46 @@ class Command(BaseCommand):
     [4] Вызывает функцию import_data_from_csv для импорта данных
     из CSV-файла в соответствующую модель.
     """
+
     help = 'Import data from CSV files'
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
+        for model, csv_file_name in MODEL_AND_CSV_MATCHING.items():
+            csv_file_name = CSV_DIR_PATH + csv_file_name
+            logger.info(f'Starting to process file: {csv_file_name}')
+            try:
+                with open(
+                    csv_file_name, newline='', encoding='utf-8'
+                ) as csv_file:
+                    self.csv_serializer(csv.DictReader(csv_file), model)
+            except Exception as e:
+                logger.error(f'Error processing file {csv_file_name}: {e}')
+                self.stdout.write(self.style.ERROR('DATA IMPORT FAILED'))
+            logger.info(f'Successfully processed file: {csv_file_name}')
+        logger.info('Successfully loaded all data into database')
+        self.stdout.write(self.style.SUCCESS('DATA IMPORTED SUCCESSFULLY'))
+
+    def csv_serializer(self, csv_data, model):
+        objects = []
+        for row_number, row in enumerate(csv_data, start=1):
+            for field in model._meta.fields:
+                if field.name in row and field.related_model:
+                    row[f'{field.name}_id'] = row.pop(field.name)
+
+            try:
+                objects.append(model(**row))
+            except Exception as e:
+                logger.error(
+                    'Error creating model instance '
+                    f'from row {row_number}: {e}'
+                )
         try:
-            # Логирование текущей директории
-            current_directory = os.getcwd()
-            logger.info(f'Current working directory: {current_directory}')
-
-            # Абсолютный путь к файлу конфигурации
-            config_file_path = os.path.join(current_directory, CONFIG_FILE)
-            logger.info(f'Configuration file path: {config_file_path}')
-
-            with open(config_file_path, 'r', encoding='utf-8') as config_file:
-                config = json.load(config_file)
-
-            for file_name in os.listdir(DIR_PATH):
-                if file_name.endswith('.csv'):
-                    file_path = os.path.join(DIR_PATH, file_name)
-                    model_name = file_name.split('.')[0].lower()
-                    model_data = config.get(model_name)
-                    if model_data:
-                        model = getattr(models, model_data['model'])
-                        fields_mapping = model_data['fields_mapping']
-                        import_data_from_csv(file_path, model, fields_mapping)
-                    else:
-                        logger.warning(
-                            f'No configuration found for model {model_name}'
-                        )
-            self.stdout.write(self.style.SUCCESS('Data imported successfully'))
+            model.objects.bulk_create(objects)
+            logger.info(
+                f'Successfully bulk created {len(objects)} '
+                f'objects for model {model.__name__}'
+            )
         except Exception as e:
-            logger.error(f'Error during the import process: {e}')
-            self.stdout.write(self.style.ERROR('Data import failed'))
+            logger.error(
+                f'Error bulk creating objects for model {model.__name__}: {e}'
+            )
