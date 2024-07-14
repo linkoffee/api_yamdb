7494317a1ba0db82ed0c22465b13495ba5f0fdb7
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-07-14 15:01:09 +0500

    fix #3 import_data script

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,92 +1,131 @@
 import os
 import csv
-import logging
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError, transaction
 
 from reviews import models
 
-# Путь до директории с csv-файлами:
+# Путь до директории с csv-файлами
 CSV_DIR_PATH = 'static/data/'
 
-# Поля, которые являются внешними ключами.
+# Поля, которые являются внешними ключами
 FOREIGN_KEY_FIELDS = ('author', 'category')
 
-# Словарь соответствий модели и csv-файла:
+# Словарь соответствий модели и csv-файла
 MODEL_AND_CSV_MATCHING = {
-    models.Category: 'category.csv',
-    models.Comment: 'comments.csv',
+    models.User: 'users.csv',
     models.Genre: 'genre.csv',
-    models.Review: 'review.csv',
+    models.Category: 'category.csv',
     models.Title: 'titles.csv',
-    models.User: 'users.csv'
+    models.GenreTitle: 'genre_title.csv',
+    models.Review: 'review.csv',
+    models.Comment: 'comments.csv',
 }
-
-# Настройка логирования:
-log_file_path = os.path.join(os.path.dirname(__file__), 'import_data.log')
-
-logging.basicConfig(
-    level=logging.INFO,
-    format=(
-        '[%(asctime)s][%(levelname)s][%(message)s][%(filename)s][%(lineno)s]'
-    ),
-    handlers=[
-        logging.FileHandler(log_file_path),
-        logging.StreamHandler()
-    ]
-)
-logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    """
-    Пользовательская команда Django.
-    --------------------------------
-    [1] Перебирает все файлы с расширением .csv в указанной директории.
-    [2] Для каждого CSV-файла определяет соответствующую модель.
-    [3] Выполняет сопоставление полей из json файла.
-    [4] Вызывает функцию import_data_from_csv для импорта данных
-    из CSV-файла в соответствующую модель.
-    """
+    """Пользовательская команда Django для импорта данных из CSV в БД."""
 
     help = 'Import data from CSV files'
 
     def handle(self, *args, **kwargs):
         for model, csv_file_name in MODEL_AND_CSV_MATCHING.items():
-            csv_file_name = CSV_DIR_PATH + csv_file_name
-            logger.info(f'Starting to process file: {csv_file_name}')
+            csv_file_path = os.path.join(CSV_DIR_PATH, csv_file_name)
+            self.stdout.write(
+                self.style.NOTICE(f'Starting to process file: {csv_file_path}')
+            )
+
+            deleted_count, _ = model.objects.all().delete()
+            self.stdout.write(
+                self.style.NOTICE(
+                    f'Deleted {deleted_count} existing '
+                    f'records from model {model.__name__}'
+                )
+            )
+
             try:
                 with open(
-                    csv_file_name, newline='', encoding='utf-8'
+                    csv_file_path, newline='', encoding='utf-8'
                 ) as csv_file:
                     self.csv_serializer(csv.DictReader(csv_file), model)
+                self.stdout.write(self.style.SUCCESS(
+                    f'Successfully processed file: {csv_file_path}'))
             except Exception as e:
-                logger.error(f'Error processing file {csv_file_name}: {e}')
-                self.stdout.write(self.style.ERROR('DATA IMPORT FAILED'))
-            logger.info(f'Successfully processed file: {csv_file_name}')
-        logger.info('Successfully loaded all data into database')
-        self.stdout.write(self.style.SUCCESS('DATA IMPORTED SUCCESSFULLY'))
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'Error processing file {csv_file_path}: {e}'
+                    )
+                )
+        self.stdout.write(
+            self.style.SUCCESS('<===SUCCESSFULLY LOADED ALL DATA INTO DB===>')
+        )
 
     def csv_serializer(self, csv_data, model):
         objects = []
         for row_number, row in enumerate(csv_data, start=1):
-            for field in model._meta.fields:
-                if field.name in row and field.related_model:
-                    row[f'{field.name}_id'] = row.pop(field.name)
+            row = self.check_foreign_keys(row, model, row_number)
+            if row is not None:
+                try:
+                    objects.append(model(**row))
+                except Exception as e:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            'Error creating model instance '
+                            f'from row {row_number}: {e}'
+                        ))
 
-            try:
-                objects.append(model(**row))
-            except Exception as e:
-                logger.error(
-                    'Error creating model instance '
-                    f'from row {row_number}: {e}'
-                )
-        try:
-            model.objects.bulk_create(objects)
-            logger.info(
-                f'Successfully bulk created {len(objects)} '
+        self.bulk_create_objects(model, objects)
+
+    def check_foreign_keys(self, row, model, row_number):
+        """Проверяет, существуют ли внешние ключи в строке данных."""
+        for field in model._meta.fields:
+            if field.name in row and field.related_model:
+                related_model = field.related_model
+                try:
+                    row[f'{field.name}_id'] = related_model.objects.get(
+                        id=row.pop(field.name)).id
+                except related_model.DoesNotExist:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f'Error: {related_model.__name__} with id '
+                            f'{row[field.name]} does not '
+                            f'exist (row {row_number})'
+                        ))
+                    return None
+        return row
+
+    def bulk_create_objects(self, model, objects):
+        """Создает объекты модели в базе данных."""
+        self.stdout.write(
+            self.style.NOTICE(
+                f'Preparing to bulk create {len(objects)} '
                 f'objects for model {model.__name__}'
-            )
+            ))
+
+        try:
+            with transaction.atomic():
+                model.objects.bulk_create(objects)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Successfully bulk created {len(objects)} '
+                    f'objects for model {model.__name__}'
+                ))
+        except IntegrityError as e:
+            self.stdout.write(
+                self.style.ERROR(
+                    'Integrity error bulk creating objects '
+                    f'for model {model.__name__}: {e}'
+                ))
         except Exception as e:
-            logger.error(
-                f'Error bulk creating objects for model {model.__name__}: {e}'
-            )
+            self.stdout.write(
+                self.style.ERROR(
+                    'Error bulk creating objects '
+                    f'for model {model.__name__}: {e}'
+                ))
+
+        created_count = model.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'{created_count} objects now exist in the '
+                f'database for model {model.__name__}'
+            ))
